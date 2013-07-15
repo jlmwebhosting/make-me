794db4976b2311e60a80d9f2c6eb88f972590dc5
Chris Williams <chris@iterativedesigns.com>
2013-07-15 18:16:13 -0400

    Adding libjsoncpp-dev dependency for MiracleGrue

--- linux.sh
+++ linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-apt-get install -y python-dev avrdude scons libcurl4-gnutls-dev rubygems
+apt-get install -y python-dev avrdude scons libcurl4-gnutls-dev rubygems libjsoncpp-dev
 gem install bundler
