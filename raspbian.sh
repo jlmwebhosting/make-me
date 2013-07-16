2738ab1a8e37661fcc643ac079f3a890c20ce030
Chris Williams <chris@iterativedesigns.com>
2013-07-16 14:15:52 -0400

    Adding more for MiracleGrue

--- raspbian.sh
+++ raspbian.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-apt-get install -y python-dev avrdude scons libcurl4-gnutls-dev rubygems libjsoncpp-dev libcppunit-dev
+apt-get install -y python-dev avrdude scons libcurl4-gnutls-dev rubygems libjsoncpp-dev libcppunit-dev libqt4-dev scons libcppunit-doc
 gem install bundler
